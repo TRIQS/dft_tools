2f7b170e782f7593b3e2162dd61471184e6f9b4c
Priyanka Seth <pseth@cpht.polytechnique.fr>
2014-11-12 16:49:49 +0100

    operators.operators2 --> operators

--- lda_dmft_cthyb.py
+++ lda_dmft_cthyb.py
@@ -4,7 +4,7 @@ import os
 import pytriqs.utility.mpi as mpi
 from itertools import *
 from pytriqs.parameters.parameters import Parameters
-from pytriqs.operators.operators2 import *
+from pytriqs.operators import *
 from pytriqs.archive import HDFArchive
 from pytriqs.applications.impurity_solvers.cthyb import *
 from pytriqs.gf.local import *
