327f100a64ee1f0222d700238ff9752ec5c6d0aa
Oleg Peil <Oleg.Peil@unige.ch>
2015-08-24 19:34:48 +0200

    Fixed the name of function 'generate_plo()'
    The function 'generate_ortho_plos()' is renamed to
    'generate_plo()', which fixes the partially faulty merge.

--- python/converters/vasp/python/main.py
+++ python/converters/vasp/python/main.py
@@ -3,7 +3,7 @@ import sys
 import vaspio
 from inpconf import ConfigParameters
 from elstruct import ElectronicStructure
-from plotools import generate_ortho_plos, output_as_text
+from plotools import generate_plo, output_as_text
 
 if __name__ == '__main__':
     narg = len(sys.argv)
@@ -23,5 +23,5 @@ if __name__ == '__main__':
     pars.parse_input()
     vasp_data = vaspio.VaspData(vasp_dir)
     el_struct = ElectronicStructure(vasp_data)
-    pshells, pgroups = generate_ortho_plos(pars, el_struct)
+    pshells, pgroups = generate_plo(pars, el_struct)
     output_as_text(pars, el_struct, pshells, pgroups)
