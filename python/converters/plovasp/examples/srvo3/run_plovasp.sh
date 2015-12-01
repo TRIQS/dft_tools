e35c6fd5c0d8032df40b104e90dc4733352ed2dd
Oleg E. Peil <Oleg.Peil@unige.ch>
2015-12-01 13:24:58 +0100

    Fixed some files in examples
    Scripts 'run_plovasp.sh' have been replaced by a template in which
    the path must be set by the user.
    Also, .gitignore has been added to example 'lunio3'.

--- python/converters/plovasp/examples/srvo3/run_plovasp.sh
+++ python/converters/plovasp/examples/srvo3/run_plovasp.sh
@@ -1,2 +1,2 @@
-PLOVASP_PATH=$HOME/Codes/triqs/triqs1.2/applications/dft_tools/dev_src
+PLOVASP_PATH=/path/to/triqs1.2/applications/dft_tools/src
 PYTHONPATH=$PLOVASP_PATH/python:$PLOVASP_PATH/c:$PYTHONPATH pytriqs $PLOVASP_PATH/python/converters/plovasp/main.py $@
