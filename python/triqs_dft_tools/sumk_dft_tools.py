5d9ee2b004ec983959e10c88d8cc6fcf947b50b9
Harrison LaBollita <hlabolli@asu.edu>
2022-10-16 11:50:23 -0400

    projected Akw fixed

--- python/triqs_dft_tools/sumk_dft_tools.py
+++ python/triqs_dft_tools/sumk_dft_tools.py
@@ -831,7 +831,7 @@ class SumkDFTTools(SumkDFT):
             subgrp=self.bands_data, things_to_read=things_to_read)
         if not value_read:
             return value_read
-        if ishell:
+        if ishell is not None:
             things_to_read = ['rot_mat_all', 'rot_mat_all_time_inv']
             value_read = self.read_input_from_hdf(
                 subgrp=self.parproj_data, things_to_read=things_to_read)
@@ -897,7 +897,7 @@ class SumkDFTTools(SumkDFT):
 
                 for ish in range(self.shells[ishell]['dim']):
                     for sp in spn:
-                        Akw[sp][ish, ik] = -gf.data[numpy.where((mesh > om_minplot)&(mesh < om_maxplot))].imag.trace(axis1=1, axis2=2)/numpy.pi
+                        Akw[sp][ish, ik] = -G_loc[sp].data[numpy.where((mesh > om_minplot)&(mesh < om_maxplot)),ish,ish].imag/numpy.pi
         # Collect data from mpi
         for sp in spn:
             Akw[sp] = mpi.all_reduce(mpi.world, Akw[sp], lambda x, y: x + y)
